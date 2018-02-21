ec0034afc037255b81cee52f8ff928a8f38b89ad
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-02-21 13:32:43 +0100

    delete the _ci folder

--- _ci/docs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Wed 21 Sep 2016 13:08:05 CEST
-
-source $(dirname ${0})/functions.sh
-
-# Deletes all existing dav folders that will be overwritten
-for k in "${DOC_UPLOADS[@]}"; do
-  dav_upload_folder sphinx "${k}"
-done

cc1ce0edb68be230c6061d9c104a666a5e12c9c0
Andre Anjos <andre.dos.anjos@gmail.com>
2018-04-19 13:39:38 +0200

    Small fixes to build script

--- before_build.sh
+++ before_build.sh
@@ -8,10 +8,13 @@ GITLAB_CHECKOUT_STRATEGY="${GITLAB_CHECKOUT_STRATEGY:-git@gitlab.idiap.ch:}"
 # Clone all packages
 for pkg in bob `cat requirements.txt | sed -e '/^\s*#.*/d;/^\s*$/d'`; do
     git clone --depth 1 ${GITLAB_CHECKOUT_STRATEGY}bob/$pkg doc/$pkg || \
-    git -C doc/$pkg clean -ffdx && \
-    git -C doc/$pkg checkout -- . && \
+    { git -C doc/$pkg reset --hard HEAD && \
     git -C doc/$pkg checkout master && \
-    git -C doc/$pkg pull
+    git -C doc/$pkg pull }
+    if [[ -n "${CI_COMMIT_TAG}" ]]; then
+      tag=`git -C doc/$pkg tag -l --sort=v:refname | tail -n 1`
+      git -C doc/$pkg checkout $tag
+    fi
 done
 
 # Create extra-intersphinx.txt
